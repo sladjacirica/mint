df647c52eae4fdce3c307b010f327ebbef6a8628
Sladjana Ćirica <djadjos@gmail.com>
2021-11-22 19:11:52 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,39 @@
+<?php include('partials/header.php');?>
+<!-- header -->
+<section class="headerBlog">
+        <img src="img/blog-header.jpg" alt="">
+        <h1 class="display-4 ">Blog</h1>
+</section>
+<!-- main blog page -->
+<section class="main container py-5">
+        <article class="row">
+            <div class="col-md-8">
+                <div class="blog">
+                    <img class="img-fluid" src="img/mint drustvene mreze.jpg" alt="">
+                    <div class="text">
+                        <h3>Drustvene mreze</h3>
+                        <p class="meta">23.09.2021 | Admin</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                        <a href="single.php">Procitaj vise</a>
+                    </div>
+                </div>
+                <div class="blog">
+                    <img class="img-fluid" src="img/mint-google-ads.jpg" alt="">
+                    <div class="text">
+                        <h3>Google Ads</h3>
+                        <p class="meta">23.09.2021 | Admin</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                        <a href="single.php">Procitaj vise</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4">
+                
+        <?php include('partials/sidebar.php');?> 
+            </div>
+        </article>
+    </section>
+
+    
+<?php include('partials/footer.php');?>
---
+++ single.php
@@ -0,0 +1,32 @@
+<?php include('partials/header.php');?>
+<!-- header -->
+<section class="headerBlog">
+        <img src="img/blog-header.jpg" alt="">
+        <h1 class="display-4">Blog</h1>
+</section>
+<!-- main blog page -->
+<section class="main container py-5">
+        <article class="row">
+            <div class="col-md-8">
+                <div class="blog single">
+                    <img class="img-fluid mb-2" src="img/mint drustvene mreze.jpg" alt="">
+                    <div class="text">
+                        <h3>Drustvene mreze</h3>
+                        <p class="meta">23.09.2021 | Admin</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                        <img class="img-fluid" src="img/mint drustvene mreze.jpg" alt="">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim error ipsa esse delectus tempore nobis, adipisci perspiciatis consequuntur, accusantium rem, numquam iste ratione odio in porro magni? Soluta repudiandae, dolor tempore porro hic labore beatae quibusdam magni quos autem. Libero.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                
+        <?php include('partials/sidebar.php');?> 
+            </div>
+        </article>
+    </section>
+
+    
+<?php include('partials/footer.php');?>
